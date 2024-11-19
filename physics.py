ab34982aa6b52c752b5a6935acfd34adc1d57785
bhall <bhall1224@gmail.com>
2024-11-18 21:53:42 -0500

    collisions work kindof

--- physics.py
+++ physics.py
@@ -1,24 +1,41 @@
-from typing import Any
-
 from loguru import logger
+from numpy.random import normal
 from pygame import Vector2
 
 ZERO_VECTOR = Vector2(0, 0)
 
 
 class PhysicsBody:
+    GRAVITY = 9.8
+
     def __init__(
-        self, mass: float = 1.0, position: Vector2 = ZERO_VECTOR, friction: float = 1.0
+        self,
+        mass: float = 1.0,
+        position: Vector2 = ZERO_VECTOR,
+        friction: float = 0.0,
+        elasticity: float = 1.0,
+        slip: float = 1.0,
     ) -> None:
         self.mass = mass
         self.velociy = ZERO_VECTOR
         self.position = position
         self.friction = friction
+        self.k_friction = 0
+        self.elasticity = elasticity
+        self.slip = slip
 
     def force(self, acceleration: Vector2, dtime: float) -> Vector2:
-        force = acceleration * self.mass * self.friction
-        self.velociy += force * dtime / self.mass
-        self.move(dtime)
+        force = acceleration * dtime
+
+        if (
+            self.k_friction == 0
+            and abs(force.magnitude()) > self.normal_force() * self.friction
+        ) or (abs(force.magnitude()) > self.normal_force() * self.k_friction):
+            if self.friction > 0 and self.k_friction == 0:
+                self.k_friction = self.friction * normal(loc=self.slip)
+            self.velociy += force
+            self.move(dtime)
+
         return self.velociy
 
     def move(self, dtime: float) -> Vector2:
@@ -30,15 +47,25 @@ class PhysicsBody:
 
         return self.position
 
-    def on_collide(self, other, dtime: float, loss: Vector2 = ZERO_VECTOR):
-        # TODO: this is where collisions happen, and it's broken lol
+    def on_collide(self, other):
+        # New velocity is given by V`= (V(m-s) + U2s)/(m+s)
+        # for initial magnitudes Vm(self) and Us(other)
         assert isinstance(other, PhysicsBody)
 
         logger.trace(f"received physics body {other}")
         logger.trace(f"my velocity: {self.velociy}")
-        acc = (other.velociy * other.mass - self.velociy * self.mass) / dtime
-        acc -= loss / dtime
-        self.force(acc, dtime)
+        self.velociy *= self.mass - other.mass
+        self.velociy += other.velociy * 2 * other.mass
+        self.velociy /= self.mass + other.mass
+
+        # energy loss, if any
+        self.velociy *= self.elasticity
+
+    def momentum(self) -> Vector2:
+        return self.velociy * self.mass
+
+    def normal_force(self) -> float:
+        return self.mass * self.GRAVITY
 
     def __str__(self) -> str:
         return str(self.position)
