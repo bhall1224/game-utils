162a83edcdc5fbba47988c8c2e14d0c8cbee8c4b
Bee Hall <bhall1224@gmail.com>
2026-06-12 18:21:04 -0400

    refactored game module into functions and decorators

---
+++ src/game_utils/game/__init__.py
@@ -0,0 +1,1 @@
+from game import main, update, NEXT_UPDATE_EVENT
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,37 +1,9 @@
 import logging
 
-import pygame.event
-from pygame.locals import K_ESCAPE, QUIT, USEREVENT
-
-from game_utils.game.game import Game
-from game_utils.screen import ScreenSettings
+from game_utils import game
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
-
-TESTEVENT = USEREVENT
 
 
-class MockGame(Game):
-    def __init__(self):
-        super().__init__()
-        self.state = 0
-        self.event = False
+print("hello world")
 
-    def update(self):
-        self.state += 1
-        if self.state == 1:
-            pygame.event.post(pygame.event.Event(TESTEVENT))
-        if self.state == 3:
-            self.running = False
-
-    def events(self, event, *args, **kwargs):
-        if event.type == TESTEVENT:
-            self.event = True
-
-
-def test_run():
-    tg = MockGame()
-    tg.run()
-    assert tg.state == 3
-    assert tg.event == True
