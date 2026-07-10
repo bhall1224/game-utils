49d728dbdf10961e831ebcf39ed81c7e5adc7f0d
Bee Hall <bhall1224@gmail.com>
2026-07-10 10:21:33 -0400

    more physics work

--- src/game_utils/physics/lib/linear_algebra.c
+++ src/game_utils/physics/lib/linear_algebra.c
@@ -6,111 +6,107 @@
 #include "linear_algebra.h"
 #endif
 
-vector create_vector(vlen *num_dimensions, float *dimensions)
+vector create_vector(vdimension *num_dimension_ptr)
 {
-    vector v = malloc(sizeof *dimensions);
-
-    for (int i = 0; i < *num_dimensions; i++)
-    {
-        v[i] = dimensions[i];
-    }
+    vector v = calloc(*num_dimension_ptr, sizeof(float));
 
     return v;
 }
 
 
-vector zero_vector(vlen *dimensions)
+void delete_vector(vector v)
 {
-    float vector_arr[*dimensions];
-
-    for (int i = 0; i < *dimensions; i++)
-    {
-        vector_arr[i] = 0.0f;
-    }
-    
-    return create_vector(dimensions, vector_arr);
+    free(v);
 }
 
 
-vector identity_vector(vlen *dimensions)
+vector zero_vector(vdimension *num_dimensions_ptr)
 {
-    float vector_arr[*dimensions];
-
-    for (int i = 0; i < *dimensions; i++)
-    {
-        vector_arr[i] = 1.0f;
-    }
-    return create_vector(dimensions, vector_arr);
+    // already initialized to 0's
+    return create_vector(num_dimensions_ptr);
 }
 
 
-vector map_vector(vlen *v_size, vector base_vector, vector_transform op)
+
+void set_v_dim_value(vdimension* ptr_dim_to_set, float *ptr_value_to_set, vector v_to_set)
 {
-    return op(v_size, base_vector);
+    v_to_set[*ptr_dim_to_set] = *ptr_value_to_set;
 }
 
-vector vector_map_list(vlen *v_size, vector base_vector, vector_transform op[]);
-
-vector copy_vector(vlen *v_size, vector v_to_copy)
+vector identity_vector(vdimension *num_dimensions_ptr)
 {
-    float v_new[*v_size];
+    float val = 1.0f;
+    vector vi = create_vector(num_dimensions_ptr);
 
-    for (int i = 0; i < *v_size; i++)
+    // populate with 1's
+    for (int i = 0; i < *num_dimensions_ptr; i++)
     {
-        v_new[i] = v_to_copy[i];
+        set_v_dim_value(num_dimensions_ptr, &val, vi);
     }
 
-    return create_vector(v_size, v_new);
+    return vi;
 }
 
-
-vector scale_vector(vlen *v_size, vector v_to_scale, float scalar)
+void map_vector(vdimension *v_size_ptr, vector v_ref_to_map, vector_transform op)
 {
-    vector new_v = copy_vector(v_size, v_to_scale);
-
-    for (int i = 0; i < *v_size; i++)
-    {
-        new_v[i] *= scalar;
-    }
-
-    return new_v;
+    op(v_size_ptr, v_ref_to_map);
 }
 
 
-vector add_vectors(vlen *v_size, vector va, vector vb)
-{    
-    vector new_v = copy_vector(v_size, va);
-
-    for (int i = 0; i < *v_size; i++)
-    {
-        new_v[i] += vb[i];
-    }
-
-    return new_v;
-}
-
-
-vector add_all_vectors(vlen *v_size, vlen *arr_size, vector vectors[])
+void vector_map_list(vdimension *v_size_ptr, vdimension *op_arr_size,  vector v_ref_to_map, vector_transform op[])
 {
-    vector sum_v = zero_vector(v_size);
-
-    for (int i = 0; i < *arr_size; i++)
+    for (int i = 0; i < *op_arr_size; i++) 
     {
-        sum_v = add_vectors(v_size, sum_v, vectors[i]);
+        op[i](v_size_ptr, v_ref_to_map);
     }
-
-    return sum_v;
 }
 
 
-float magnitude(vlen *v_size, vector v)
+void scale_vector(vdimension *v_size_ptr, vector v_ref_to_scale, float *scalar_ptr)
+{
+    for (int i = 0; i < *v_size_ptr; i++)
+    {
+        v_ref_to_scale[i] *= *scalar_ptr;
+    }
+}
+
+
+void add_vectors(vdimension *v_size_ptr, vector v_ref_to_set, vector v_ref_to_use)
+{
+    for (int i = 0; i < *v_size_ptr; i++)
+    {
+        v_ref_to_set[i] += v_ref_to_use[i];
+    }
+}
+
+
+void add_all_vectors(vdimension *v_size_ptr, vdimension *arr_size_ptr, vector v_to_set, vector vctrs_to_sum[])
+{
+    for (int i = 0; i < *arr_size_ptr; i++)
+    {
+        add_vectors(v_size_ptr, v_to_set, vctrs_to_sum[i]);
+    }
+}
+
+
+void copy_and_release_vector(vdimension *v_size_ptr, vector v_to_set, vector v_cpy_rls)
+{
+    for (int i = 0; i < *v_size_ptr; i++)
+    {
+        v_to_set[i] = v_cpy_rls[i];
+    }
+
+    delete_vector(v_cpy_rls);
+}
+
+float magnitude(vdimension *v_size_ptr, vector vector)
 {
     // sqrt(sum(vx^2 in v))
     float mag = 0.0f;
 
-    for (int i = 0; i < *v_size; i++)
+    for (int i = 0; i < *v_size_ptr; i++)
     {
-        mag += pow((double)v[i], 2.0);
+        mag += pow((double)vector[i], 2.0);
     }
 
     mag = (float)sqrt((double)mag);
@@ -118,12 +114,15 @@ float magnitude(vlen *v_size, vector v)
     return mag;
 }
 
-short int compare_vectors(vlen *v_size, vector va, vector vb)
+short int compare_vectors(vdimension *v_size_ptr, vector vctr_a, vector vctr_b)
 {
-    float ma = magnitude(v_size, va);
-    float mb = magnitude(v_size, vb);
+    float ma = magnitude(v_size_ptr, vctr_a);
+    float mb = magnitude(v_size_ptr, vctr_b);
 
-    if (ma > mb) return 1;
-    else if (ma == mb) return 0;
-    else return -1;
+    if (ma > mb)
+        return 1;
+    else if (ma == mb)
+        return 0;
+    else
+        return -1;
 }
--- src/game_utils/physics/lib/linear_algebra.h
+++ src/game_utils/physics/lib/linear_algebra.h
@@ -1,15 +1,20 @@
-typedef unsigned int vlen;
+typedef unsigned int vdimension;
 typedef float *vector;
-typedef vector (*vector_transform)(vlen*, vector);
+typedef void (*vector_transform)(vdimension*, vector);
 
-vector create_vector(vlen *num_dimensions, float *dimensions);
-vector zero_vector(vlen *dimensions);
-vector identity_vector(vlen *dimensions);
-vector map_vector(vlen *v_size, vector base_vector, vector_transform op);
-vector vector_map_list(vlen *v_size, vector base_vector, vector_transform op[]);
-vector scale_vector(vlen *v_size, vector v_to_scale, float scalar);
-vector add_vectors(vlen *v_size, vector va, vector vb);
-vector add_all_vectors(vlen *v_size, vlen *arr_size, vector vectors[]);
-vector copy_vector(vlen *v_size, vector v_to_copy);
-float magnitude(vlen *v_size, vector v);
-short int compare_vectors(vlen *v_size, vector va, vector vb);
+vector create_vector(vdimension *num_dimension_ptr);
+void delete_vector(vector v);
+
+vector zero_vector(vdimension *num_dimensions_ptr);
+vector identity_vector(vdimension *num_dimensions_ptr);
+
+void set_v_dim_value(vdimension* ptr_dim_to_set, float *ptr_value_to_set, vector v_to_set);
+void map_vector(vdimension *v_size_ptr, vector v_ref_to_map, vector_transform op);
+void vector_map_list(vdimension *v_size_ptr, vdimension *op_arr_size,  vector v_ref_to_map, vector_transform op[]);
+void scale_vector(vdimension *v_size_ptr, vector v_ref_to_scale, float *scalar_ptr);
+void add_vectors(vdimension *v_size_ptr, vector v_ref_to_set, vector v_ref_to_use);
+void add_all_vectors(vdimension *v_size_ptr, vdimension *arr_size_ptr, vector v_to_set, vector vctrs_to_sum[]);
+void copy_and_release_vector(vdimension *v_size_ptr, vector v_to_set, vector v_cpy_rls);
+
+float magnitude(vdimension *v_size_ptr, vector vector);
+short int compare_vectors(vdimension *v_size_ptr, vector vctr_a, vector vctr_b);
